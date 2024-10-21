c5c80237fe0ef0832f0900b927bf8dcfb50dcaee
juliochirindza <132257103+juliochirindza@users.noreply.github.com>
2024-10-21 07:41:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session id",
+SESSION_ID: process.env.SESSION_ID || "LaAkHJSL#2wfWW7BeXWzDtBHcZ_lNcvwcB1i8NFYfvBs69BRPGFA",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/1ece2e0281513c05d20ee.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🎉 Hi ɱʀㅤʝɑyk Is Online Now 💻\n*💻 Owner* - DARK-SHADOW-MD\n\n*💻 Owner Number* -258843617938",
